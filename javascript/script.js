2663e23070e70069cf37a3370faca99316792a42
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-06-08 20:22:57 -0400

    modified API call to a new API, added API call to script.js, function to toggle purchase form started

--- javascript/script.js
+++ javascript/script.js
@@ -6,19 +6,49 @@ $( document ).ready(function() {
     alert("Yo! Testing the JavaScript");
 
 
-// Pull current movies from database and display to HTML
+    // Grab values from "movie search text-box"
+    var movieSearch = $("#search_movies_input");
 
-// Grab values from "movie search text-box"
-var movieSearch = $("#search_movies_input").val().trim();
+    // Grab values from "location text-box"
+    var movieLocation = $("#search_location_input");
+    
+    // Grab values from "ratings dropdown-box"
 
-// Grab values from "location text-box"
-var movieLocation = $("#search_location_input").val().trim();
-// Grab values from "ratings text-box"
-// Display movies based on above parameters
-$("#submit_button").on("click", function(){
-    console.log(movieSearch, movieLocation);
-    $("#main_content").text(movieSearch, movieLocation);
-});
+    // Movie submit button function
+    $("#submit_button").on("click", function(){
+        var movieValue = movieSearch.val().trim();
+        var locationValue = movieLocation.val().trim();
+        console.log(movieValue);
+        console.log(locationValue);
+    });
+
+    // Pull current movies from database
+    jQuery.ajax({
+        url: "https://api.internationalshowtimes.com/v4/movies/",
+        type: "GET",
+        data: {
+            "countries": "US",
+        },
+        headers: {
+            "X-API-Key": "KBvReF0P6MlqDF9zeORmnpIrGRictjlU",
+        },
+    })
+    .done(function(data, textStatus, jqXHR) {
+        console.log("HTTP Request Succeeded: " + jqXHR.status);
+        console.log(data);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("HTTP Request Failed");
+    })
+    .always(function() {
+        /* ... */
+    });
+    
+ 
+
+
+
+
 
 /* when user selects a movie, display movie data:
     - Film poster
@@ -32,13 +62,15 @@ $("#submit_button").on("click", function(){
 // Display seating chart with available seating
 // user can select a seat by clicking on one that is available
 
-/* After seat selection, display purchase form:
-    - Name
-    - eMail
-    - Payment info
-
-    Submit button
-*/
+// Toggle purchase form
+    $("#seating_submit").on("click", function() {
+        var paymentForm = $("#payment_form");
+        if (paymentForm.style.display === "none") {
+            paymentForm.style.display = "block";
+        } else {
+            paymentForm.style.display = "none";
+        }
+    });
 
 // On submit button press, display thank you screen
 
