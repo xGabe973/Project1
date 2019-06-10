b7db40201630ca89531f90a7c3b49b0f96d8a4ff
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-06-09 23:11:07 -0400

    created ajax request as a seperate function and invoked within submit button

--- javascript/script.js
+++ javascript/script.js
@@ -7,20 +7,24 @@ $(document).ready(function() {
     // Movie submit button function
     $("#submit_button").on("click", function(event){
         event.preventDefault();
-        var movieSearch = $("#search_movies_input").val().trim();
-        var movieLocation = $("#search_location_input").val().trim();
-        var movieRating = $("#select_rating :selected").val();
+        let movieSearch = $("#search_movies_input").val().trim();
+        let movieLocation = $("#search_location_input").val().trim();
+        let movieRating = $("#select_rating :selected").val();
+
         console.log(movieSearch);
         console.log(movieLocation);
         console.log(movieRating);
+
+        getMovie(movieSearch);
+    });
     
-        // Pull current movies from database
+    // Function to pull movie from database
+    function getMovie(movieSearch) {
         jQuery.ajax({
             url: "https://api.internationalshowtimes.com/v4/movies/",
             type: "GET",
             data: {
                 "countries": "US",
-
             },
             headers: {
                 "X-API-Key": "KBvReF0P6MlqDF9zeORmnpIrGRictjlU",
@@ -28,20 +32,23 @@ $(document).ready(function() {
         })
         .done(function(data, textStatus, jqXHR) {
             console.log("HTTP Request Succeeded: " + jqXHR.status);
-            console.log(data.movies);
+            console.log(data);
 
+            /*
             $.each(data.movies, function(i, val) {
                 var movieTitle = val.title;
                 console.log(movieTitle);
+                
                 if (movieSearch = movieTitle) {
                     $("#movie_title").text(movieTitle);
                 } else { 
                     $("#movie_title").text("Movie not found");
-                }
+                } 
                 
                 var movieIMG = val.poster_image_thumbnail;
                 console.log(movieIMG);
             })
+            */
         
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
@@ -50,16 +57,8 @@ $(document).ready(function() {
         .always(function() {
             /* ... */
         });
-    });
-
-
-/* when user selects a movie, display movie data:
-    - Film poster
-    - MPAA film rating
-    - Brief description
-    - Runtime
-    - User ratings and reviews
-*/
+    };
+    
 
 
 // Display seating chart with available seating
